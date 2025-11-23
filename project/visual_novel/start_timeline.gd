e1bd52f5a37e6df820f5679842036665b6200464
Axistorm <gael.laguna@epitech.eu>
2025-11-23 09:54:46 +0100

    fix (cutscene.tscn): make the cutscene run + what comes after

--- project/visual_novel/start_timeline.gd
+++ project/visual_novel/start_timeline.gd
@@ -4,24 +4,25 @@ func _ready() -> void:
 	Dialogic.signal_event.connect(_input)
 	Dialogic.timeline_ended.connect(_on_timeline_ended)
 
-# Called when the node enters the scene tree for the first time.
+
 func _input(_event: InputEvent) -> void:
-	# check if a dialog is already running
-	if Global.start_cutscene == true:
+	if Global.start_cutscene:
 		call_deferred("_do_cutscene")
+		return
 	
 	if Dialogic.current_timeline != null:
 		return
-	if Dialogic.current_timeline == null:
-		Dialogic.start(Global.chapter_name)
-		get_viewport().set_input_as_handled()
-		
+	
+	Dialogic.start(Global.chapter_name)
+	Global.go_to_menu = true
+	get_viewport().set_input_as_handled()
 
 func _on_timeline_ended():
-	Dialogic.timeline_ended.disconnect(_on_timeline_ended)
-	get_tree().change_scene_to_file("res://project/menu/main.tscn")
+	if Dialogic.timeline_ended.is_connected(_on_timeline_ended):
+		Dialogic.timeline_ended.disconnect(_on_timeline_ended)
+	if Global.go_to_menu:
+		get_tree().change_scene_to_file("res://project/menu/main.tscn")
 
-func _do_cuscene():
-	Dialogic.end_timeline()
+func _do_cutscene():
 	Global.start_cutscene = false
-	get_tree().change_scene_to_file("res://project/visual_novel/video_stream_player.tscn")
+	get_tree().change_scene_to_file("res://project/visual_novel/cutscene.tscn")
